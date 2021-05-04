577dccd4a6cd206819580d63aac37c320dbba9c9
Naman Joshi <41015546+N-Joshi@users.noreply.github.com>
2021-05-04 09:24:46 +0530

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,17 @@
+#Given a string python, try printing cython using slicing [start:stop] and concatenation. +
+
+name = 'python';
+
+print('c'+name[1:name.__len__()])
+
+ 
+
+ 
+
+#Given any file python file (like something.py) use slicing to
+
+filename = 'something.py'
+
+print('Extenstion  : ' + filename[-2:filename.__len__()])
+
+print('File Name   : ' + filename[0:-3])
